927c519ce4e69ec25bfcd9d47aae8dfbf26f5a5b
Yasmin Neumann <136760513+Yasmin-min@users.noreply.github.com>
2023-11-22 21:55:10 -0300

    Update ProgramaPrincipal.java

--- ProgramaPrincipal.java
+++ ProgramaPrincipal.java
@@ -3,8 +3,8 @@ package jogo;
 import java.util.Scanner;
 
 public class ProgramaPrincipal {
+	
 	static Scanner entrada = new Scanner(System.in);
- static Scanner teclado = new Scanner(System.in);
 	public static void main(String[] args) {
 		MenuLogin login = new MenuLogin();
 		
@@ -12,22 +12,22 @@ public class ProgramaPrincipal {
 		
 		int opcao = menuInicial();
 		
-		while (opcao!= 0){
+		while (true){
 			switch (opcao) {
 			case 1:
 				MenuLogin.menu(); 
 				break;
 			case 2:
-				System.out.println("pontuação!");
+				System.out.println("pontuacao!"); //Pontuacao();
 				break;
 			case 0:
 				System.out.println("Obrigada por jogar!");
 				break;
 			default: 
-				System.out.println("Valor inválido!"); 
+				System.out.println("Valor invalido!"); 
 				break; 
 			}
-			opcao = menuInicial();
+			break;
 		}	
 	}
 	public static int menuInicial() {
@@ -36,16 +36,16 @@ public class ProgramaPrincipal {
 		while (true) {
 		System.out.println("\n-----MENU-----\n"
 				+ "\n1. Entrar"
-				+ "\n2. Ver historico de pontuação"
+				+ "\n2. Ver historico de pontuacao"
 				+ "\n0. Sair");
-		System.out.print("Digite a sua opção: ");
+		System.out.print("Digite a sua opcao: ");
 		opcao = entrada.nextInt();
 		if (opcao <0 && opcao >3) {
-			System.out.println("Opção inválida!");
+			System.out.println("Opcao invalida!");
 		}else {
 		return opcao; 
 		}
 		}
 	}
-		
+
 }
