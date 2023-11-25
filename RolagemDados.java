035837cadf93be7981bfbf5962df8f86c87cdb3f
Will OIiveira <105466766+BiGSerial@users.noreply.github.com>
2023-11-25 15:50:03 -0300

    Separação da classe de de rolagem
    Separação da Classe de Rolagem

---
+++ RolagemDados.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import main.yahtzee.utils.Dado;
+
+public class RolagemDados {
+    private List<Integer> valoresDosDados;
+    private Dado dado;
+
+    public RolagemDados() {
+        valoresDosDados = new ArrayList<>();
+        dado = new Dado();
+    }
+
+    public List<Integer> rolarDados() {
+        valoresDosDados.clear();
+
+        for (int i = 0; i < 5; i++) {
+            int valorDoDado = dado.getValor();
+            valoresDosDados.add(valorDoDado); // Adiciona o valor gerado à lista
+        }
+
+        return valoresDosDados;
+    }
+}
